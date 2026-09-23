4a60e3dd27fdd420003a9cab079913e4c62f3ebc
Shaan Nabi <shaanchess2004@gmail.com>
2026-09-23 13:26:20 -0400

    Added SQL_Lab2.sql and worked on Doodlewoodle.sql & .cpp

--- DoodleWoodle.sql
+++ DoodleWoodle.sql
@@ -1,6 +1,13 @@
-CREATE TABLE inventory (
-    product_name VARCHAR(30),
-    product_ID CHAR(10),
-    units INT(8),
-    cost DECIMAL(3,2)
-);
+create table instructor
+	(
+	 ID varchar(5), 
+	 name varchar(20) not null, 
+	 dept_name varchar(20), 
+	 salary numeric(8,2) check (salary > 29000),
+	 primary key (ID),
+	 foreign key (dept_name) references department (dept_name)
+		on delete set null
+	);
+
+INSERT INTO instructor 
+VALUES (‘78910’, ‘Hinton’, ‘Comp. Sci.’ 1000000.00);
